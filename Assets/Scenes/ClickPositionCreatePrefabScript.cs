323a52c4a8ef48c1e7f58e81e3a00bb8e58b8c84
kikuti10q <onyosat@gmail.com>
2021-10-19 14:51:18 +0900

    リンゴの当たり判定の追加とランダム生成の追加

--- Assets/Scenes/ClickPositionCreatePrefabScript.cs
+++ Assets/Scenes/ClickPositionCreatePrefabScript.cs
@@ -1,16 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ClickPositionCreatePrefabScript : MonoBehaviour
 {
 	// 生成したいPrefab
 	public GameObject Prefab;
+
+	public List<GameObject> prefabs;
+
 	// クリックした位置座標
 	private Vector3 clickPosition;
 	// Use this for initialization
 	void Start()
 	{
 
+	}
+
+	private GameObject GetRandom() {
+		return prefabs[Random.Range(0, prefabs.Count)];
 	}
 
 	// Update is called once per frame
@@ -24,9 +32,10 @@ public class ClickPositionCreatePrefabScript : MonoBehaviour
 			clickPosition = Input.mousePosition;
 			// Z軸修正
 			clickPosition.z = 10f;
+			var randomPrefab = GetRandom();
 			// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
 			// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
-			Instantiate(Prefab, Camera.main.ScreenToWorldPoint(clickPosition), Prefab.transform.rotation);
+			Instantiate(randomPrefab, Camera.main.ScreenToWorldPoint(clickPosition), randomPrefab.transform.rotation);
 		}
 	}
 }
