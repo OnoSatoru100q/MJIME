7dd451fbfa6c1456706649aa5a7a9ccb4eff4a74
masa5956 <92581382+masa5956@users.noreply.github.com>
2021-10-30 03:34:42 +0900

    リンゴのテクスチャバグを修正
    リンゴのテクスチャバグを修正した
    
    メニューに行くボタンを作成中いじらないように

---
+++ Assets/Scenes/script/CP.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CPt : MonoBehaviour
+{
+	// 生成したいPrefab
+	public GameObject Prefab;
+
+	public List<GameObject> prefabs;
+
+
+	// クリックした位置座標
+	private Vector3 clickPosition;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	private GameObject GetRandom()
+	{
+		return prefabs[Random.Range(0, prefabs.Count)];
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// マウス入力で左クリックをした瞬間
+		if (Input.GetMouseButtonDown(0))
+		{
+			// ここでの注意点は座標の引数にVector2を渡すのではなく、Vector3を渡すことである。
+			// Vector3でマウスがクリックした位置座標を取得する
+			clickPosition = Input.mousePosition;
+			// Z軸修正
+			clickPosition.z = 10f;
+			var randomPrefab = GetRandom();
+			// オブジェクト生成 : オブジェクト(GameObject), 位置(Vector3), 角度(Quaternion)
+			// ScreenToWorldPoint(位置(Vector3))：スクリーン座標をワールド座標に変換する
+			Instantiate(randomPrefab, Camera.main.ScreenToWorldPoint(clickPosition), randomPrefab.transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scenes/script/ff.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ff : MonoBehaviour
+{
+    //Buttonを格納する変数
+    public Canvas Menue;
+    
+
+    
+    // Use this for initialization
+    void Start()
+    {
+        //初めはBottonをオフにしておく
+        Menue.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        //もしButtonがオフだったら
+        if (!Menue.enabled)
+        {
+            //サブカメラをオンにする
+            Menue.enabled = true;
+
+        }
+        //もしButtonがオンだったら
+        else
+        {
+            //Buttoをオフにする
+            Menue.enabled = false;
+            //サブカメラ１をオンにする
+
+        }
+    }
+}
