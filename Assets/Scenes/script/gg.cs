320bbc885e199aeaa662de5e57a6fe87ebd936ea
masa5956 <92581382+masa5956@users.noreply.github.com>
2021-10-31 06:12:00 +0900

    メニュー画面に行くボタンの追加
    メニュー画面に行くボタンを追加した
    細かい内容はスラックに貼ってある

---
+++ Assets/Scenes/script/gg.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gg : MonoBehaviour
+{
+    //Buttonを格納する変数
+    public Canvas Menue1;
+    public Canvas Menue;
+    public Canvas Canvas;
+    public GameObject Cube;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //初めはBottonをオフにしておく
+        Menue1.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        //もしButtonがオフだったら
+        if (!Menue1.enabled)
+        {
+            //サブカメラをオンにする
+            Menue1.enabled = true;
+            
+            Menue.enabled = false;
+            Canvas.enabled = false;
+
+        }
+      
+    }
+}
---
+++ Assets/Scenes/script/ii.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ii : MonoBehaviour
+{
+    //Buttonを格納する変数
+   
+    public Canvas Menue;
+    
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        //もしButtonがオンだったら
+        if (Menue.enabled)
+        {
+            //サブカメラをオフにする
+            Menue.enabled = false;
+            
+            
+
+
+        }
+
+    }
+}
