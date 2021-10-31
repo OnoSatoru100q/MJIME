024ec14e37e5889579d154aae37681c48c6bf9c2
masa5956 <92581382+masa5956@users.noreply.github.com>
2021-10-31 21:28:39 +0900

    BGMを追加した

---
+++ Assets/Scenes/script/oo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class oo : MonoBehaviour
+{
+    //Buttonを格納する変数
+    public Canvas Menue1;
+    public Canvas Next;
+    public Canvas Canvas;
+    
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //初めはBottonをオフにしておく
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    //ボタンを押した時の処理
+    public void PushButton()
+    {
+        //もしButtonがオフだったら
+        if (Next.enabled)
+        {
+            //サブカメラをオンにする
+            Next.enabled = false;
+
+            Menue1.enabled = false;
+
+            Canvas.enabled = true;
+
+
+        }
+
+    }
+}
