86dea907b8efe6d096027eb517ff0e43972ff58b
masa5956 <92581382+masa5956@users.noreply.github.com>
2021-11-18 21:31:11 +0900

    リプレイボタン

---
+++ Assets/script/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Button : MonoBehaviour
+{
+    public void replay()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int buildIndex = scene.buildIndex;
+        SceneManager.LoadScene(buildIndex);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
